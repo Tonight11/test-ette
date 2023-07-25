e919afc484ef9b7dd393c49ef33323498f671af9
tonight11 <godofspeedman@gmail.com>
2023-07-25 22:17:14 +0300

    feat: added header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 	import BookingForm from './components/BookingForm.vue';
+	import TheHeader from './components/TheHeader.vue';
 </script>
 
 <template>
+	<TheHeader />
 	<main class="book">
 		<h1 class="book__title title">Добавление объекта. Аренда</h1>
 		<BookingForm />
 	</main>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.book {
+		padding: 20px;
+	}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<script setup>
+	import IconsBurger from './icons/IconsBurger.vue';
+</script>
+
+<template>
+	<header class="header">
+		<div class="header__burger">
+			<IconsBurger />
+		</div>
+		<div class="header__avatar">Баястан</div>
+	</header>
+</template>
+
+<style scoped>
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 20px;
+		border-radius: 0px 0px 24px 24px;
+		background: #edf3ff;
+		box-shadow: 0px 5px 11px 0px rgba(140, 178, 240, 0.13);
+	}
+	.header__burger {
+		cursor: pointer;
+	}
+
+	.header__avatar {
+		font-size: 14px;
+		font-weight: 600;
+	}
+</style>
