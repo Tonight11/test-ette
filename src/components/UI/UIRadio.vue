479fa3c29615a260c37b21b898c608c2f61a7f9f
tonight11 <godofspeedman@gmail.com>
2023-07-25 20:52:02 +0300

    feat: radio comp

---
+++ src/components/UI/UIRadio.vue
@@ -0,0 +1,19 @@
+<template>
+	<input
+		type="radio"
+		:value="value"
+		:checked="isChecked"
+		@change="$emit('update:modelValue', $event.target.value)"
+	/>
+</template>
+
+<script setup>
+	import { computed, defineProps } from 'vue';
+
+	const props = defineProps(['value', 'modelValue']);
+	defineEmits(['update:modelValue']);
+
+	const isChecked = computed(() => {
+		return props.modelValue === props.value;
+	});
+</script>
